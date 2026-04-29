09bf194b774b88575dde205f6eee3e3cf2a0025b
Piyush Dutt <piyushdutt102007@gmail.com>
2026-04-29 06:32:26 +0530

    Enhance book management with existence checks
    Updated book management functions to handle existing books and improved user prompts.

--- dict lib.py
+++ dict lib.py
@@ -1,52 +1,60 @@
-books={}
-issued_books={}
+books = {}
+issued_books = {}
 
 def add_books():
-    name=input("Enter book name:")
-    books[name]="available"
-    print("Book added")
+    name = input("Enter book name: ")
+    if name in books:
+        print("Book already exists")
+    else:
+        books[name] = "available"
+        print("Book added")
 
 def show_books():
-    if books=={}:
+    if books == {}:
         print("No books available")
     else:
         print("Books:")
         for i in books:
-            if books[i]=="available":
-                print(i,"-available")
-            else:
-                print(i,"-issued")
+            print(i, "=", books[i])
 
 def issue_books():
-    name=input("Enter book name:")
-    if name in books and books[name]=="available":
-        student=input("Enter student name:")
-        days=int(input("For how many days:"))
-        books[name]="issued"
-        issued_books[name]=[student,days]
-        print("Book issued")
+    name = input("Enter book name: ")
+    if name in books:
+        if books[name] == "available":
+            student = input("Enter student name: ")
+            days = int(input("For how many days: "))
+            if days > 0:
+                books[name] = "issued"
+                issued_books[name] = [student, days]
+                print("Book issued")
+            else:
+                print("Invalid days")
+        else:
+            print("Book already issued")
     else:
-        print("Book not available")
+        print("Book not found")
 
 def calculate_fine(extra):
-    fine=0
-    for i in range(1,extra+1):
-        fine=fine+(10*i)
+    fine = 0
+    for i in range(1, extra + 1):
+        fine = fine + (10 * i)
     return fine
 
 def return_books():
-    name=input("Enter book name:")
+    name = input("Enter book name: ")
     if name in issued_books:
-        used=int(input("Enter days used:"))
-        allowed=issued_books[name][1]
-        if used>allowed:
-            extra=used-allowed
-            fine=calculate_fine(extra)
+        used = int(input("Enter days used: "))
+        allowed = issued_books[name][1]
+
+        if used > allowed:
+            extra = used - allowed
+            fine = calculate_fine(extra)
             print("Late return")
-            print("Fine:",fine)
+            print("Fine:", fine)
         else:
             print("Returned on time")
-        books[name]="available"
+
+        books[name] = "available"
         issued_books.pop(name)
         print("Book returned")
     else:
@@ -54,24 +62,27 @@ def return_books():
 
 def library():
     while True:
-        print("1.Add book")
-        print("2.Show books")
-        print("3.Issue book")
-        print("4.Return book")
-        print("5.Exit")
-        choice=input("Enter choice:")
-        if choice=="1":
+        print("1 Add book")
+        print("2 Show books")
+        print("3 Issue book")
+        print("4 Return book")
+        print("5 Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
             add_books()
-        elif choice=="2":
+        elif choice == "2":
             show_books()
-        elif choice=="3":
+        elif choice == "3":
             issue_books()
-        elif choice=="4":
+        elif choice == "4":
             return_books()
-        elif choice=="5":
+        elif choice == "5":
             print("Exit")
             break
         else:
             print("Wrong choice")
 
-library()
+library()  
+is this pk??
